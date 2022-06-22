cb84ac6f0ad8028f1ecc562576e4ccb8927184bc
Leonardo Blanski <leo_blanski@hotmail.com>
2022-06-21 21:38:51 -0300

    Tema vueX aula 288 finalizada

--- src/store/store.js
+++ src/store/store.js
@@ -5,10 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        produto: [
-            { nome: 'Produto 1', quantidade: 7, preco: 14.55 },
-            { nome: 'Produto 2', quantidade: 10, preco: 22.99 },
-            { nome: 'Produto 3', quantidade: 1, preco: 43.18 },
-        ],
+        produtos: [],
+    },
+    getters: {
+        valorTotal(state) {
+            return state.produtos.map(p => p.quantidade * p.preco)
+                .reduce((total, atual) => total + atual, 0)
+        }
     }
 });
